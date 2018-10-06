f858c8611d9a961ffe7d2240101820e986765d21
OscarGB <oscar.gomez.borz@gmail.com>
2018-10-06 19:09:27 +0200

    mas templates

--- P2/html/index.py
+++ P2/html/index.py
@@ -13,6 +13,22 @@ def carrito():
 def pagar():
     return render_template("pagar.html")
 
+@app.route("/contacto/")
+def contacto():
+    return render_template("contacto.html")
+
+@app.route("/login/")
+def login():
+    return render_template("login.html")
+
+@app.route("/register/")
+def register():
+    return render_template("register.html")
+
+@app.route("/user-info/")
+def user_info():
+    return render_template("user-info.html")
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
 

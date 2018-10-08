318ef4c66b0ed0fd98e6f13789f8ca090f7d2610
OscarGB <oscar.gomez.borz@gmail.com>
2018-10-08 16:09:56 +0200

    Creo que todos los templates en básico, queda meterle todo lo que son variables y esas mierdas

--- P2/html/index.py
+++ P2/html/index.py
@@ -29,6 +29,22 @@ def register():
 def user_info():
     return render_template("user-info.html")
 
+@app.route("/listado_peliculas/")
+def listado_peliculas():
+    return render_template("listado_peliculas.html")
+
+@app.route("/categorias/")
+def categorias():
+    return render_template("categorias.html")
+
+@app.route("/categorias/<categoria>/")
+def categorias_categoria(categoria):
+    return render_template("categoria.html", Titulo=categoria)
+
+@app.route("/peliculas/<pelicula>/")
+def pelicula(pelicula):
+    return render_template("pelicula.html", Titulo=pelicula)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
 

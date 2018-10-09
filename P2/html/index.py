9f3652b48c37407f1424a8be23985971620cccee
OscarGB <oscar.gomez.borz@gmail.com>
2018-10-09 13:10:00 +0200

    Plantilla pelicula creo que funciona, sidebar completo

--- P2/html/index.py
+++ P2/html/index.py
@@ -1,71 +1,91 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template
+from random import shuffle
+import json,os
+
+
+
+Data = open(os.path.dirname(__file__)+"/peliculas.json", "r").read()
+Data = json.loads(Data)
+aux = list(set([a["categoria"] for a in Data["peliculas"]]))
+Categorias = []
+for a in aux:
+    auxdic = {"nombre": a, "imagen": None}
+    for film in Data["peliculas"]:
+        if (film["categoria"] == a):
+            auxdic["imagen"] = film["poster"]
+            break
+    Categorias.append(auxdic)
+Novedades = sorted(Data["peliculas"], key=lambda x: -int(x["anno"]))
+def sort_rec(x):
+    aux = 0.0
+    for a in x["opiniones"]:
+        aux += float(a["puntuacion"])
+    return -aux/len(x["opiniones"])
+Recomendadas = sorted(Data["peliculas"], key=lambda x: sort_rec(x))
+
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    destacadas = [
-    {"nombre":u"Acción", "imagen":"gladiator.jpg"},
-    {"nombre":u"Animación", "imagen":"del_reves.jpg"},
-    {"nombre":u"Comedia", "imagen":"ocho_apellidos_vascos.jpg"},
-    {"nombre":u"Ficción", "imagen":"avatar.jpg"},
-    {"nombre":u"Musical", "imagen":"la_la_land.jpg"},
-    {"nombre":u"Drama", "imagen":"titanic.jpg"}
-    ]
-    novedades = [
-    {"nombre":u"Indiana Jones", "imagen":"indiana_jones.jpg"},
-    {"nombre":u"Indiana Jones 2", "imagen":"indiana_jones2.jpg"},
-    {"nombre":u"Campeones", "imagen":"campeones.jpg"},
-    {"nombre":u"Lo Imposible", "imagen":"lo_imposible.jpg"},
-    {"nombre":u"La La Land", "imagen":"la_la_land.jpg"},
-    {"nombre":u"os increibles 2", "imagen":"los_increibles2.jpg"},
-    {"nombre":u"Ocho Apellidos Vascos", "imagen":"ocho_apellidos_vascos.jpg"},
-    {"nombre":u"Bajo La Misma Estrella", "imagen":"bajo_la_misma_estrella.jpg"},
-    {"nombre":u"El gran Showman", "imagen":"el_gran_showman.jpg"},
-    {"nombre":u"Del Revés", "imagen":"del_reves.jpg"},
-    ]
-    return render_template("index.html", destacadas=destacadas, novedades=novedades)
+    return render_template("index.html",\
+     novedades_sidebar=Novedades[:4], populares_sidebar=Recomendadas[:4],\
+     destacadas=Categorias, novedades=Novedades[:10])
 
 @app.route("/carrito/")
 def carrito():
-    return render_template("carrito.html")
+    return render_template("carrito.html",\
+     novedades_sidebar=Novedades[:4], populares_sidebar=Recomendadas[:4])
 
 @app.route("/pagar/")
 def pagar():
-    return render_template("pagar.html")
+    return render_template("pagar.html",\
+     novedades_sidebar=Novedades[:4], populares_sidebar=Recomendadas[:4])
 
 @app.route("/contacto/")
 def contacto():
-    return render_template("contacto.html")
+    return render_template("contacto.html",\
+     novedades_sidebar=Novedades[:4], populares_sidebar=Recomendadas[:4])
 
 @app.route("/login/")
 def login():
-    return render_template("login.html")
+    return render_template("login.html",\
+     novedades_sidebar=Novedades[:4], populares_sidebar=Recomendadas[:4])
 
 @app.route("/register/")
 def register():
-    return render_template("register.html")
+    return render_template("register.html",\
+     novedades_sidebar=Novedades[:4], populares_sidebar=Recomendadas[:4])
 
 @app.route("/user-info/")
 def user_info():
-    return render_template("user-info.html")
+    return render_template("user-info.html",\
+     novedades_sidebar=Novedades[:4], populares_sidebar=Recomendadas[:4])
 
 @app.route("/listado_peliculas/")
 def listado_peliculas():
-    return render_template("listado_peliculas.html")
+    return render_template("listado_peliculas.html",\
+     novedades_sidebar=Novedades[:4], populares_sidebar=Recomendadas[:4])
 
 @app.route("/categorias/")
 def categorias():
-    return render_template("categorias.html")
+    return render_template("categorias.html",\
+     novedades_sidebar=Novedades[:4], populares_sidebar=Recomendadas[:4])
 
 @app.route("/categorias/<categoria>/")
 def categorias_categoria(categoria):
-    return render_template("categoria.html", Titulo=categoria)
+    return render_template("categoria.html",\
+     Titulo=categoria, novedades_sidebar=Novedades[:4], populares_sidebar=Recomendadas[:4])
 
 @app.route("/peliculas/<pelicula>/")
 def pelicula(pelicula):
-    return render_template("pelicula.html", Titulo=pelicula)
+    Peli = [a for a in Data["peliculas"] if a["titulo"] == pelicula][0]
+    Similares = [a for a in Data["peliculas"] if Peli["categoria"] == a["categoria"]]
+
+    return render_template("pelicula.html",\
+     Titulo=pelicula, novedades_sidebar=Novedades[:4], populares_sidebar=Recomendadas[:4],\
+     pelicula=Peli, similares=Similares[:8])
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)

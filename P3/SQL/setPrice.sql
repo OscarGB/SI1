314f5ea1e091ecdb7c97c626e1dfe826ca2aed5e
OscarGB <oscar.gomez.borz@gmail.com>
2018-11-13 16:38:34 +0100

    set Price con inflacion

--- P3/SQL/setPrice.sql
+++ P3/SQL/setPrice.sql
@@ -1,4 +1,4 @@
 UPDATE public.detallespedidos
-SET preciototal = detallespedidos.cantidad * productos.precio
-FROM productos
-WHERE productos.productoid = detallespedidos.productoid;
+SET preciototal = detallespedidos.cantidad * T.precio / POW(1.02, extract(year from NOW()) - extract(year from pedidos.fecha)) 
+FROM (productos NATURAL JOIN peliculas) AS T, pedidos
+WHERE T.productoid = detallespedidos.productoid and pedidos.pedidoid = detallespedidos.pedidoid;

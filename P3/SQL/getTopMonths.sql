47c721c2fe396af33a6d21d37c6b96bdab63a004
lucicolpe <lucia.colmenarejo@estudiante.uam.es>
2018-11-27 21:54:09 +0100

    memoria 3 falta updInventory

--- P3/SQL/getTopMonths.sql
+++ P3/SQL/getTopMonths.sql
@@ -3,10 +3,10 @@ DROP FUNCTION IF EXISTS getTopMonths(integer, integer);
 CREATE OR REPLACE FUNCTION getTopMonths(p integer, i integer) RETURNS TABLE(ano double precision, mes double precision, preciototal integer, cantidad integer) AS $$
 BEGIN
 	RETURN QUERY (SELECT * FROM (SELECT extract(year from fecha) as ano, extract(month from fecha) as mes, 
-					cast(SUM(T2.preciototal) AS integer) AS preciototal, cast(SUM(T2.cantidad) AS integer) AS cantidad
-		FROM pedidos AS T1, detallespedidos AS T2
-		WHERE T1.pedidoid = T2.pedidoid
-		GROUP BY ano, mes) AS T
-		WHERE T.preciototal >= i OR T.cantidad >= p);
+										cast(SUM(T2.preciototal) AS integer) AS preciototal, cast(SUM(T2.cantidad) AS integer) AS cantidad
+								FROM pedidos AS T1, detallespedidos AS T2
+								WHERE T1.pedidoid = T2.pedidoid
+								GROUP BY ano, mes) AS T
+								WHERE T.preciototal >= i OR T.cantidad >= p);
 END;
 $$ language plpgsql;

aae8b4330df7ab4182f61c6933eff85f94a90121
OscarGB <oscar.gomez.borz@gmail.com>
2018-11-28 12:17:45 +0100

    updinventory actualizado y capturas

--- P3/SQL/updInventory.sql
+++ P3/SQL/updInventory.sql
@@ -5,9 +5,11 @@ create or replace FUNCTION updInventory_function() RETURNS trigger AS $$
 BEGIN
 	IF (OLD.estado is null and NEW.estado is not null) THEN
 		UPDATE productos
-		SET stock = stock - cantidad
+		SET stock = stock - cantidad, ventas = ventas + cantidad
 		FROM detallespedidos
 		WHERE OLD.pedidoid = detallespedidos.pedidoid and detallespedidos.productoid = productos.productoid;
+		DELETE FROM alertas 
+		WHERE productoid in (SELECT productoid FROM detallespedidos where OLD.pedidoid = detallespedidos.pedidoid);
 		INSERT INTO alertas
 		SELECT productoid, stock
 		FROM (SELECT * FROM pedidos where pedidos.pedidoid = OLD.pedidoid) AS T inner join detallespedidos on detallespedidos.pedidoid = T.pedidoid natural join productos
